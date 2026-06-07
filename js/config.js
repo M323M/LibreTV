f04f57a6497b471a165aab1d577995f213c60c10
M323M <1517168159@qq.com>
2026-06-07 22:59:46 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -112,44 +112,44 @@ const API_SITES = {
     },
     ckzy: {
          api: 'https://www.ckzy1.com',
-         name: 'CK资源',
+         name: 'CK资源18+',
          
      },
      jkun: {
          api: 'https://jkunzyapi.com',
-         name: 'jkun资源',
-         adult: true
+         name: 'jkun资源18+',
+        
      },
      bwzy: {
          api: 'https://api.bwzym3u8.com',
-         name: '百万资源',
-         adult: true
+         name: '百万资源18+',
+         
      },
      souav: {
          api: 'https://api.souavzy.vip',
-         name: 'souav资源',
-         adult: true
+         name: 'souav资源18+',
+        
      },
      r155: {
          api: 'https://155api.com',
-         name: '155资源',
-         adult: true
+         name: '155资源18+',
+         
      },
      lsb: {
          api: 'https://apilsbzy1.com',
-     name: 'lsb资源',
-         adult: true
+     name: 'lsb资源18+',
+         
      },
      huangcang: {
          api: 'https://hsckzy.vip',
-         name: '黄色仓库',
-         adult: true,
+         name: '黄色仓库18+',
+        
          detail: 'https://hsckzy.vip'
      },
      yutu: {
          api: 'https://yutuzy10.com',
-         name: '玉兔资源',
-         adult: true
+         name: '玉兔资源18+',
+         
      },
 
     testSource: {
